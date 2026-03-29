797da6ba58a4731ba71e02a6f415153c80ab4ee1
Satvik Sharma <sharmasatvik031@gmail.com>
2026-03-29 15:10:59 +0530

    fix: remove problematic app.options wildcard causing PathError

--- server/server.js
+++ server/server.js
@@ -63,9 +63,6 @@ app.use(
   })
 );
 
-// Handle preflight requests
-app.options("*", cors());
-
 app.use(compression());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
